b89ff6f02738c9be44c9aeb8c4b49c82b11f3334
DDLoi <dinhducloi477@gmail.com>
2025-04-21 19:18:08 +0700

    Home page

---
+++ resources/views/products/list.blade.php
@@ -0,0 +1,18 @@
+<section class="my-5">
+    <div class="container">
+        <h2 class="mb-3"> Top Deals</h2>
+        <div class="row">
+            <div class="col-lg-3 my-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('images/products/ip15.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+
+                        <h6 class="card-title text-center">Iphone 15</h6>
+                        <h5 class="card-title text-center">$ 499.00</h5>
+                        <a href="#" class="btn btn-primary">Detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/home', [HomeController::class, 'home']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
